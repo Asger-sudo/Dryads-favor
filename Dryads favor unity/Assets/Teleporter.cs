dd6d02934aea2f002f229d5acf5530d75d3de899
caspernerving <caspernerving@gmail.com>
2025-09-30 12:21:43 +0200

    Færdigt spil

---
+++ Dryads favor unity/Assets/Teleporter.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Teleporter : MonoBehaviour
+{
+    [SerializeField] private Transform destination;
+
+    public Transform GetDestination()
+    {
+        return destination;
+    }
+}
