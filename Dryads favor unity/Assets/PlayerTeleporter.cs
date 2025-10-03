e8580f56331cc543d582be3ae2d066a1b2a3e63c
Asger <xwing20@outlook.dk>
2025-10-03 12:57:37 +0200

    teleporter fix

--- Dryads favor unity/Assets/PlayerTeleporter.cs
+++ Dryads favor unity/Assets/PlayerTeleporter.cs
@@ -3,25 +3,20 @@ using UnityEngine;
 public class playerTeleport : MonoBehaviour
 {
     private GameObject currentTeleporter;
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            if (currentTeleporter != null)
-            {
-                Debug.Log("Teleporting!");
-                transform.position = currentTeleporter.GetComponent<Teleporter>().GetDestination().position;
-            }
-        }
-
-
-    }
+    private float lastTeleportTime = -10f; // Start så man kan teleportere med det samme
+    public float teleportCooldown = 10f;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Teleporter"))
         {
-            currentTeleporter = collision.gameObject;
+            if (Time.time - lastTeleportTime >= teleportCooldown)
+            {
+                currentTeleporter = collision.gameObject;
+                Debug.Log("Teleporting!");
+                transform.position = currentTeleporter.GetComponent<Teleporter>().GetDestination().position;
+                lastTeleportTime = Time.time;
+            }
         }
     }
 
@@ -34,6 +29,5 @@ public class playerTeleport : MonoBehaviour
                 currentTeleporter = null;
             }
         }
-
     }
 }
